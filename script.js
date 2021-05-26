c91587c6776722364c7054ed50bbe98bd2d2f215
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-05-27 03:00:24 +0500

    working on it

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
     };
   });
 
- 
+  // Add White Background color to navbar when scroll down
+
 $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
@@ -25,3 +26,31 @@ $(document).ready(function(){
         }
     })
   })
+
+  // Arrow  button onClick Function scrolldown 
+
+  $(document).ready(function(){
+    // Add scrollspy to <body>
+    $('body').scrollspy({target:".content", offset: 50});   
+  
+    // Add smooth scrolling on all links inside the navbar
+    $("#section02 a").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      }  // End if
+    });
+  });
